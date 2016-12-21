156087a7c98027640a69c8165ae9da2f8cc26afb
Chris Peikert <cpeikert@alum.mit.edu>
2016-12-21 17:27:29 -0500

    temp commit, playing with depth ideas

--- alchemy/Crypto/Alchemy/EDSL.hs
+++ alchemy/Crypto/Alchemy/EDSL.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
@@ -12,87 +13,99 @@
 
 module Crypto.Alchemy.EDSL where
 
-import Crypto.Lol
--- why doesn't Lol export Tensor type?
-import Crypto.Lol.Cyclotomic.Tensor
+import Crypto.Lol                      hiding (Pos (..))
+import Crypto.Lol.Applications.SymmSHE
 
-import Data.Constraint
+import Data.Type.Natural as Nat
+
 
 class Lambda expr where
+  -- | Abstract a Haskell function.
   lam :: (expr a -> expr b) -> expr (a -> b)
+
+  -- | Apply an abstraction.
   app :: expr (a -> b) -> expr a -> expr b
 
--- CJP: design question: do we go with stronger single-argument
--- SymPT/CT classes, which support very general methods that must work
--- for all m, m' etc., subject to constraints?  It then becomes a bit
--- tricky to know what constraints are appropriate for arbitrary
--- instances.  (We could define them on a per-instance basis as
--- associated types, but this is uuuuugly.)
---
--- Or do we go with weaker classes that include m, m' etc. as
--- arguments?  This makes it harder/impossible to define methods that
--- "switch arguments" (like moduli).  One would have to define
--- multiple classes that had all relevant arguments as parameters.
+-- | Lambda annotated by depth in its argument.
+newtype LamD (d :: Nat) a b = LamD { unLamD :: (a -> b) }
+
+applyD :: LamD d a b -> a -> b
+applyD = ($) . unLamD
 
 
--- | Symantics for plaintext operations.
 
-class SymPT (expr :: Factored -> * -> *) where
-  type SymPTElt expr zp :: Constraint -- yuck, but necessary?
+-- | Depth-annotated lambda abstraction and application.
+class LambdaD expr where
+  -- | Abstract a function.
+  lamD :: (LamD d (expr a) (expr b)) -> expr (LamD d a b)
 
-  -- type PTOf expr (m :: Factored) zp :: *
+  -- | Apply an abstraction.
+  appD :: expr (LamD d a b) -> expr a -> expr b
 
-  -- litPT :: PTOf expr m zp -> expr m zp
 
-  -- CJP: might be nicer to get these via entailment
-  addPT, mulPT :: (Fact m, SymPTElt expr zp) =>
-                  expr m zp -> expr m zp -> expr m zp
+-- | Symantics for plaintext operations, associated with
+-- multiplicative depth.
+
+class SymPT expr where
+
+  addPT :: (rp ~ Cyc t m zp, Fact m, CElt t zp) =>
+           -- rp -> rp -> rp, depth 0 in both args
+           LamD N0 (expr rp) (LamD N0 (expr rp) (expr rp))
+
+  mulPT :: (rp ~ Cyc t m zp, Fact m, CElt t zp) =>
+           -- rp -> rp -> rp, depth 1 in both args
+           LamD N1 (expr rp) (LamD N1 (expr rp) (expr rp))
 
 -- | Symantics for ciphertext operations
+
+-- CJP: should this be associated with mult depth as well?  Not clear
+-- that we need it anymore.
 class SymCT expr where
 
-  -- CJP: entailment?
-  addCT, mulCT :: (e ~ expr m' zq m zp, m `Divides` m') =>
-                  e -> e -> e
-
-  {- How to handle RescaleCyc constraint? Associated constraint type instead?
-  rescaleCT :: (RescaleCyc (Cyc t) zq zq') =>
-               expr m zp m' zq -> expr m zp m' zq'
-
-  ksQuadCirc :: (ct ~ expr m zp m' zq,
-                 KeySwitchCtx gad t m' zp zq zq') =>
-                KSQuadCircHint gad (Cyc t m' zq') -> ct -> ct
-  -}
+  addCT, mulCT :: (ct ~ CT m zp (Cyc t m' zq))
+                  => expr ct -> expr ct -> expr ct
 
 
--- | Metacircular evaluator (analogous to identity monad).
+
+-- | Metacircular evaluator.
 newtype I a = I { unI :: a }
 
--- | Metacircular lambda abstraction and application.
 instance Lambda I where
-  lam f = I $ unI . f . I
-  app (I f) (I a) = I $ f a
+  lam f   = I $ unI . f . I
+  app f a = I $ (unI f) (unI a)
+
+-- | Metacircular lambda abstraction and application.
+instance LambdaD I where
+  lamD f   = I $ unI . f . I
+  appD f a = I $ f a
+
+-- | Metacircular ring operations.
+instance SymPT I where
+  addPT = LamD (\a -> LamD (\b -> a + b))
+  mulPT = LamD (\a -> LamD (\b -> a * b))
 
 
--- | Naive plaintext evaluator.
-newtype EvalPT t m zp = EPT { unEPT :: Cyc t m zp }
+-- CJP: already defined somewhere?
+type family Drop (d :: Nat) (xs :: [k]) where
+  Drop 'Z xs = xs
+  Drop ('S d) (x ': rest) = Drop d rest
 
-instance (Tensor t) => SymPT (EvalPT t) where
-  type SymPTElt (EvalPT t) zp = CElt t zp
+-- | The type of a ciphertext expression corresponding to a
+-- depth-annotated plaintext expression.
 
-  addPT (EPT a) (EPT b) = EPT $ a + b
-  mulPT (EPT a) (EPT b) = EPT $ a * b
+type family PT2CT (m' :: Factored) (zqs :: [*]) (d :: Nat) a where
+  PT2CT m' (zq ': _) d (Cyc t m zp) = CT m zp (Cyc t m' zq)
 
--- PROBLEM! Can't make EvalPT or (EvaltPT t) a meaningful instance of
--- Lambda because the kinds don't meaningfully match up
+  -- CJP: the 'Z here is arbitrary, and is likely wrong...
+  PT2CT m' zqs l (a -> b) = PT2CT m' (Drop l zqs) 'Z a -> PT2CT m' zqs l b
 
--- | Plaintext-to-ciphertext transformer.
-data PT2CT expr (m' :: Factored) zq (m :: Factored) zp where
-  P2C :: (m `Divides` m') => expr m' zq m zp -> PT2CT expr m' zq m zp
 
-instance SymCT expr => SymPT (PT2CT expr m' zq) where
-  type SymPTElt (PT2CT expr m' zq) zp = () -- nothing needed?
-
-  addPT (P2C a) (P2C b) = P2C $ addCT a b
-  mulPT (P2C a) (P2C b) = P2C $ mulCT a b
+-- | Plaintext to ciphertext compiler.
+newtype C
+  ctexpr -- ^ symantics of target ciphertext expression
+  m'     -- ^ ciphertext index
+  zqs    -- ^ typelist of (increasing) ciphertext moduli (head is root)
+  d      -- ^ depth of the plaintext expression
+  a      -- ^ type of the plaintext expression
+  = C (ctexpr (PT2CT m' zqs d a))
 
