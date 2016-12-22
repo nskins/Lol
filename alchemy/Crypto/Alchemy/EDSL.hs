b54b2e0d6a96a40af68a4046ae0262d4796d3c5a
Chris Peikert <cpeikert@alum.mit.edu>
2016-12-22 16:21:20 -0500

    progress on leveled Lambda and toy compiler

--- alchemy/Crypto/Alchemy/EDSL.hs
+++ alchemy/Crypto/Alchemy/EDSL.hs
@@ -15,97 +15,120 @@ module Crypto.Alchemy.EDSL where
 
 import Crypto.Lol                      hiding (Pos (..))
 import Crypto.Lol.Applications.SymmSHE
+import Crypto.Lol.Types
 
-import Data.Type.Natural as Nat
+-- not using TypeLits because they don't seem to be a good fit (no
+-- Peano rep)
 
+--import Data.Promotion.Prelude
+--import Data.Singletons.TypeLits
+
+import Data.Type.Natural
+
+-- | Lambda abstraction and application.
 
 class Lambda expr where
-  -- | Abstract a Haskell function.
+  -- | Abstract a Haskell function into the object language.
   lam :: (expr a -> expr b) -> expr (a -> b)
 
   -- | Apply an abstraction.
   app :: expr (a -> b) -> expr a -> expr b
 
--- | Lambda annotated by depth in its argument.
-newtype LamD (d :: Nat) a b = LamD { unLamD :: (a -> b) }
 
-applyD :: LamD d a b -> a -> b
-applyD = ($) . unLamD
+data Level (l :: Nat) a = L { unL :: a }
 
 
+-- | Lambda abstraction and application for leveled computations.
 
--- | Depth-annotated lambda abstraction and application.
-class LambdaD expr where
-  -- | Abstract a function.
-  lamD :: (LamD d (expr a) (expr b)) -> expr (LamD d a b)
+class LambdaL expr where
+  -- | Abstract a Haskell function that takes a leveled input.
+  lamL :: (expr (Level l a) -> expr b) -> expr (Level l a -> b)
 
   -- | Apply an abstraction.
-  appD :: expr (LamD d a b) -> expr a -> expr b
+  appL :: expr (Level l a -> b) -> expr (Level l a) -> expr b
 
 
--- | Symantics for plaintext operations, associated with
--- multiplicative depth.
+-- | Symantics for leveled plaintext operations.
 
 class SymPT expr where
 
-  addPT :: (rp ~ Cyc t m zp, Fact m, CElt t zp) =>
-           -- rp -> rp -> rp, depth 0 in both args
-           LamD N0 (expr rp) (LamD N0 (expr rp) (expr rp))
+  -- | Plaintext addition.  Inputs must be at the same level as
+  -- outputs.
+  addPT :: ( -- (l :<= l1) ~ 'True, (l :<= l2) ~ 'True,
+           -- CJP: generalize input levels?
+            rp ~ Cyc t m zp, Fact m, CElt t zp) =>
+           expr (Level l rp) -> expr (Level l rp) -> expr (Level l rp)
 
-  mulPT :: (rp ~ Cyc t m zp, Fact m, CElt t zp) =>
-           -- rp -> rp -> rp, depth 1 in both args
-           LamD N1 (expr rp) (LamD N1 (expr rp) (expr rp))
+  -- | Plaintext multiplication.  Inputs must be one level higher than
+  -- output.
+  mulPT :: ( -- (l :< l1) ~ 'True, (l :< l2) ~ 'True,
+           -- CJP: generalize input levels?
+            rp ~ Cyc t m zp, Fact m, CElt t zp) =>
+           expr (Level ('S l) rp) -> expr (Level ('S l) rp) -> expr (Level l rp)
+
+
+-- CJP: should the following be leveled as well?  Don't think we need
+-- it here.
 
 -- | Symantics for ciphertext operations
 
--- CJP: should this be associated with mult depth as well?  Not clear
--- that we need it anymore.
 class SymCT expr where
 
-  addCT, mulCT :: (ct ~ CT m zp (Cyc t m' zq))
-                  => expr ct -> expr ct -> expr ct
+  addCT, mulCT :: (ct ~ CT m zp (Cyc t m' zq)) =>
+                  expr ct -> expr ct -> expr ct
 
-
+  rescaleCT :: (RescaleCyc (Cyc t) (a,b) b, ToSDCtx t m' zp (a,b)) =>
+               expr (CT m zp (Cyc t m' (a,b))) -> expr (CT m zp (Cyc t m' b))
 
 -- | Metacircular evaluator.
 newtype I a = I { unI :: a }
 
+-- | Metacircular lambda abstraction and application.
 instance Lambda I where
   lam f   = I $ unI . f . I
-  app f a = I $ (unI f) (unI a)
+  app f a = I $ unI f (unI a)
 
 -- | Metacircular lambda abstraction and application.
-instance LambdaD I where
-  lamD f   = I $ unI . f . I
-  appD f a = I $ f a
+instance LambdaL I where
+  lamL f   = I $ unI . f . I
+  appL f a = I $ unI f (unI a)
+
+unIL :: I (Level l a) -> a
+unIL = unL . unI
 
 -- | Metacircular ring operations.
 instance SymPT I where
-  addPT = LamD (\a -> LamD (\b -> a + b))
-  mulPT = LamD (\a -> LamD (\b -> a * b))
+  addPT a b = I $ L $ unIL a + unIL b
+  mulPT a b = I $ L $ unIL a * unIL b
 
+{- not needed if C is a GADT
 
--- CJP: already defined somewhere?
-type family Drop (d :: Nat) (xs :: [k]) where
-  Drop 'Z xs = xs
-  Drop ('S d) (x ': rest) = Drop d rest
+type family PT2CT (m' :: Factored) (qs :: [*]) a where
+  PT2CT m' qs (Level l (Cyc t m zp))  = CT m zp (Cyc t m' (Collect l qs))
+  PT2CT m' qs (a -> b)                = PT2CT m' qs a -> PT2CT m' qs b
 
--- | The type of a ciphertext expression corresponding to a
--- depth-annotated plaintext expression.
+-}
 
-type family PT2CT (m' :: Factored) (zqs :: [*]) (d :: Nat) a where
-  PT2CT m' (zq ': _) d (Cyc t m zp) = CT m zp (Cyc t m' zq)
-
-  -- CJP: the 'Z here is arbitrary, and is likely wrong...
-  PT2CT m' zqs l (a -> b) = PT2CT m' (Drop l zqs) 'Z a -> PT2CT m' zqs l b
-
+-- | Collect the first @n+1@ moduli into nested pairs of 'ZqBasic's,
+-- representing a product ring
+type family ZqProd n qs where
+  ZqProd 'Z     (q ': _)  = (ZqBasic q Int64)
+  ZqProd ('S n) (q ': qs) = (ZqBasic q Int64, ZqProd n qs)
 
 -- | Plaintext to ciphertext compiler.
-newtype C
+data C
   ctexpr -- ^ symantics of target ciphertext expression
   m'     -- ^ ciphertext index
-  zqs    -- ^ typelist of (increasing) ciphertext moduli (head is root)
-  d      -- ^ depth of the plaintext expression
+  qs     -- ^ typelist of individual moduli
   a      -- ^ type of the plaintext expression
-  = C (ctexpr (PT2CT m' zqs d a))
+  where
+    CCT  :: ctexpr (CT m zp (Cyc t m' (ZqProd l qs)))
+         -> C ctexpr m' qs (Level l (Cyc t m zp))
+    CLam :: (C ctexpr m' qs a -> C ctexpr m' qs b)
+         -> C ctexpr m' qs (a -> b)
 
+instance (SymCT ctexpr) => SymPT (C ctexpr m' qs) where
+  addPT (CCT a) (CCT b) = CCT $ addCT a b
+
+  -- need keyswitch too
+  mulPT (CCT a) (CCT b) = CCT $ mulCT (rescaleCT a) (rescaleCT b)
